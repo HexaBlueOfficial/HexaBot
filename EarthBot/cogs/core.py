f38340818f98421dd7da15474ffc084c5ebb27cd
Flamey <morellialex08@gmail.com>
2021-04-19 18:16:30 +0200

    Autopublish

--- EarthBot/cogs/core.py
+++ EarthBot/cogs/core.py
@@ -25,6 +25,12 @@ class Core(commands.Cog):
     async def before_presence(self):
         await self.bot.wait_until_ready()
     
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        channels = [832658521731498005, 832659080841134110, 832659031847993344, 832659442213453871, 832660792397791262, 832661047398760450, 832671013753454602]
+        if message.channel.id in channels:
+            await message.publish()
+    
     @commands.command(name="info")
     async def info(self, ctx):
         """Shows information about Earth."""
