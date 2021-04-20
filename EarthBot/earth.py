5eb395af9bd5cb10fdb4c3f995e51586763d5cbe
Flamey <morellialex08@gmail.com>
2021-04-20 19:17:04 +0200

    Maybe load the cog next time

--- EarthBot/earth.py
+++ EarthBot/earth.py
@@ -24,7 +24,7 @@ async def on_command_error(ctx, error):
     await handled.delete()
     raise error
 
-extensions = ["cogs.core", "cogs.help", "cogs.utility", "jishaku"]
+extensions = ["cogs.core", "cogs.fun", "cogs.help", "cogs.utility", "jishaku"]
 for extension in extensions:
     bot.load_extension(extension)
 
