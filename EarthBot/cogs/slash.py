e6711b7114a8a471cad4cf8332f5d9567b52f18d
Flamey <morellialex08@gmail.com>
2021-06-03 18:37:32 +0200

    v1.2.2: Polls.

--- EarthBot/cogs/slash.py
+++ EarthBot/cogs/slash.py
@@ -50,10 +50,10 @@ class Slash(commands.Cog):
         e = discord.Embed(title="About Earth", color=0x00a8ff, description="**Earth** is a private bot for the server **Planet Earth**. It has a few fun commands to keep you entertained while it also does more serious stuff.")
         e.set_author(name="Earth", icon_url="https://this.is-for.me/i/gxe1.png")
         e.set_thumbnail(url="https://this.is-for.me/i/gxe1.png")
-        e.add_field(name="Developers", value="<@450678229192278036>: `/info`, AutoPublish, AutoPing, `/say`, `/uwu`, `/cat`, `/dog`, `/fox`, `/hug`, `/kill`, `/gaypercent`, `/8ball`, `/ping`, `/uptime`, `/userinfo`, `/serverinfo`, `/nitro`.\n<@598325949808771083>: `/help`.\nOther: `/jishaku` (External Extension).", inline=False)
+        e.add_field(name="Developers", value="<@450678229192278036>: `/info`, AutoPublish, AutoPing, `/say`, `/uwu`, `/cat`, `/dog`, `/fox`, `/hug`, `/kill`, `/gaypercent`, `/8ball`, `/poll`, `/ping`, `/uptime`, `/userinfo`, `/serverinfo`, `/nitro`.\n<@598325949808771083>: `/help`.\nOther: `/jishaku` (External Extension).", inline=False)
         if luckyint == 69:
-            e.set_field_at(0, name="Developers", value="<@450678229192278036>: `/info`, AutoPublish, AutoPing, `e.arth`, `/say`, `/uwu`, `/cat`, `/dog`, `/fox`, `/hug`, `/kill`, `/gaypercent, `/8ball`, `/ping`, `/uptime`, `/userinfo`, `/serverinfo`, `/nitro`.\n<@598325949808771083>: `/help`.\nOther: `/jishaku` (External Extension).", inline=False)
-        e.add_field(name="Versions", value=f"Earth: v1.2.1\ndiscord.py: v{discord.__version__}", inline=False)
+            e.set_field_at(0, name="Developers", value="<@450678229192278036>: `/info`, AutoPublish, AutoPing, `e.arth`, `/say`, `/uwu`, `/cat`, `/dog`, `/fox`, `/hug`, `/kill`, `/gaypercent, `/8ball`, `/poll`, `/ping`, `/uptime`, `/userinfo`, `/serverinfo`, `/nitro`.\n<@598325949808771083>: `/help`.\nOther: `/jishaku` (External Extension).", inline=False)
+        e.add_field(name="Versions", value=f"Earth: v1.2.2\ndiscord.py: v{discord.__version__}", inline=False)
         e.add_field(name="Credits", value="**Hosting:** [Library of Code](https://loc.sh/discord)\n**Inspiration for `/kill`, `/gaypercent` and `/8ball`:** [Dank Memer](https://dankmemer.lol) bot.\n**Inspiration for `/uwu`:** [Reddit UwUtranslator bot](https://reddit.com/u/uwutranslator)\n**Cats:** [TheCatAPI](https://thecatapi.com)\n**Dogs:** [TheDogAPI](https://thedogapi.com)\n**Foxes:** [Random Fox](https://randomfox.ca)", inline=False)
         e.set_footer(text="Earth by Earth Development", icon_url="https://this.is-for.me/i/gxe1.png")
         await ctx.send(embed=e)
@@ -338,6 +338,33 @@ class Slash(commands.Cog):
         e.add_field(name="The 8 Ball's Answer", value=f"{ballline}", inline=False)
         e.set_footer(text="Earth by Earth Development", icon_url="https://this.is-for.me/i/gxe1.png")
         await ctx.send(embed=e)
+    
+    @slashcog.cog_slash(name="poll", description="Create a poll. Currently only supports two options.", options=[
+        slash.utils.manage_commands.create_option("name", "The poll's name.", 3, True),
+        slash.utils.manage_commands.create_option("option1", "The first option to vote on.", 3, True),
+        slash.utils.manage_commands.create_option("option2", "The second option to vote on.", 3, True)
+    ])
+    async def _poll(self, ctx: slash.SlashContext, name: str, option1: str, option2: str):
+        vote1 = 0
+        vote2 = 0
+
+        e = discord.Embed(title=f"Poll: {name}", color=0x00a8ff, description=f"**Poll by {ctx.author.mention}.**\nThink and choose. You cannot change your answer.")
+        e.set_author(name="Earth", icon_url="https://this.is-for.me/i/gxe1.png")
+        e.add_field(name="Votes", value=f"`{option1} (1)`: {vote1}\n`{option2} (2)`: {vote2}", inline=False)
+        e.set_footer(text="Earth by Earth Development", icon_url="https://this.is-for.me/i/gxe1.png")
+        poll = await ctx.send(embed=e, components=[
+            slash.utils.manage_components.create_button(slash.utils.manage_components.ButtonStyle.blue, "1st Option"),
+            slash.utils.manage_components.create_button(slash.utils.manage_components.ButtonStyle.blue, "2nd Option")
+        ])
+
+        waitfor1 = await self.bot.wait_for("button_click", check=lambda r: r.component.label.startswith("1st"))
+        if waitfor1.channel == ctx.channel:
+            vote1 += 1
+            await poll.edit(content=None, embed=e)
+        waitfor2 = await self.bot.wait_for("button_click", check=lambda r: r.component.label.startswith("2nd"))
+        if waitfor2.channel == ctx.channel:
+            vote2 += 1
+            await poll.edit(content=None, embed=e)
     
     @slashcog.cog_slash(name="uptime", description="Shows an Embed with Earth's uptime.")
     async def _uptime(self, ctx: slash.SlashContext):
